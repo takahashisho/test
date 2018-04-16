5360570adb1b09006d8dfe309aa956296b18d83d
Rush-light <lightning_o-din0711@docomo.ne.jp>
2018-04-16 21:44:23 +0900

    0416

--- WebProj/src/com/internousdev/webproj/action/LoginAction.java
+++ WebProj/src/com/internousdev/webproj/action/LoginAction.java
@@ -11,6 +11,9 @@ public class LoginAction extends ActionSupport {
 	public String getUsername() {
 		return username;
 	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
 	public String getPassword() {
 		return password;
 	}
